ce933e0b1144736e4176a30b2d40618ea8bae1a0
JC <cimbulis.j@gmail.com>
2020-11-28 18:02:37 +0200

    mic check 1 2

---
+++ xplat/Shared/xplatApp.swift
@@ -0,0 +1,17 @@
+//
+//  xplatApp.swift
+//  Shared
+//
+//  Created by Janis Cimbulis on 28/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct xplatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
